95764b14ece2060b167eb986c99c2c1aa21b194e
Guilherme Souza <gss214@hotmail.com>
2022-02-28 01:35:08 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
 from time import sleep
-from unittest import result
 import tweepy
-import os
 
-api_key = os.environ.get('API_KEY')
-api_secret_key = os.environ.get('API_SECRET_KEY')
-access_token = os.environ.get('ACCESS_TOKEN')
-access_token_secret = os.environ.get('ACESS_TOKEN_SECRET')
-bearer_token = os.environ.get('BEARER_TOKEN')
+
+api_key = '10ZJkrP9fGSO8UC2cNYRII440'
+api_secret_key = 'TDAociKwrPlTd1N5xYIOsjo7OuglikV9V4bc8NeXGtH0lGIINB'
+access_token = '1497819855508189201-Qda53dd4zHQugTt5bgp3kjNIEzI6tS'
+access_token_secret = 'CcsirWJ410MK62a6mUlxofdkXxbOLqz8Nv4FVNoOoy5pg'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAGu9ZgEAAAAAXeUkYtbWXSEgaQAZZzQUC6sYw1g%3DSDyRqiB5JBuZGvUR9Opyw1fXWGs7mi5hrXTZkx745IzUfZPCG2'
 
 auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, access_token_secret)
@@ -22,8 +21,9 @@ def fav_retweet_coque():
                 api.retweet(tweet.id)
                 print(f'Coque bot retweetou: {tweet.text}')
         except Exception as e:
-            print(e)
+            pass
 
 while True:
     fav_retweet_coque()
     sleep(15)
+    
